96b44d53b550ed2daaa0161a578673355c5eb2f8
gface34rus <89259112970@mail.ru>
2024-08-13 22:06:53 +0300

    Homework is done!

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import static tasks.Task1.printOddNumbers;
+
 public class Main {
     public static void main(String[] args) {
+        /*   Задача1   */
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        printOddNumbers(nums);
+        System.out.println();
+        /*   Задача2   */
+
     }
 }
---
+++ src/tasks/Task1.java
@@ -0,0 +1,13 @@
+package tasks;
+
+import java.util.List;
+
+public class Task1 {
+    public static void printOddNumbers(List<Integer> nums) {
+        for (Integer num : nums) {
+            if (num % 2 != 0) {
+                System.out.println(num);
+            }
+        }
+    }
+}
